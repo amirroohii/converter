c2ab15ccd16812dcadaa24b8931873b0ea293bcd
IMRUUHI\roohi <roohiamir931@gmail.com>
2024-10-06 21:40:25 +0330

    1.0.7

--- Converter/urls.py
+++ Converter/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('image-converter/', include('image_converter.urls')),
-    path('file-converter/', include('file_converter.urls'))
+    path('file-converter/', include('file_converter.urls')),
+    path('merge-pdf/', include('merge_pdf.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
